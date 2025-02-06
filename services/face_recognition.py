21bcb1864fafdc040982aade63349596ff9575cf
rodrigo-rosario <digolht@icloud.com>
2025-02-05 21:54:59 -0300

    git add .

---
+++ services/face_recognition.py
@@ -0,0 +1,40 @@
+import os
+import cv2
+import dlib
+import numpy as np
+
+# Obtendo o caminho absoluto da pasta onde está o script
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Caminhos absolutos para os modelos
+shape_predictor_path = os.path.join(BASE_DIR, "shape_predictor_68_face_landmarks.dat")
+face_rec_model_path = os.path.join(BASE_DIR, "dlib_face_recognition_resnet_model_v1.dat")
+
+# Carregar os modelos do Dlib
+detector = dlib.get_frontal_face_detector()
+shape_predictor = dlib.shape_predictor(shape_predictor_path)
+face_rec_model = dlib.face_recognition_model_v1(face_rec_model_path)
+
+def extract_embeddings(image):
+    """
+    Extrai os embeddings faciais da imagem.
+    """
+    try:
+        if image is None:
+            return np.array([])
+        
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # Converter para escala de cinza
+        faces = detector(gray)  # Detectar rosto
+
+        if len(faces) == 0:
+            return np.array([])  # Nenhum rosto detectado
+        
+        for face in faces:
+            shape = shape_predictor(gray, face)
+            embedding = face_rec_model.compute_face_descriptor(image, shape)
+            return np.array(embedding) / np.linalg.norm(embedding)  # Normalizar vetor
+        
+        return np.array([])  # Caso nenhum rosto seja processado
+    except Exception as e:
+        print(f"Erro na extração de embeddings: {e}")
+        return np.array([])  # Retorna vazio em caso de erro
--- services:image_processing.py
+++
@@ -1,9 +0,0 @@
-import cv2 
-def validate_image(image):
-    '''
-    Valida se a imagem carregada e  valida e possui conteudo
-
-    '''
-    if image is None or image.shape[0] == 0 or image.shape[1] == 0:
-        return False
-    return True
